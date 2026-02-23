125305a16d1b7640193f02312bdfb695f1d74d8d
NicoNeko <nicuch100@gmail.com>
2026-02-23 19:04:16 +0200

    Fix issue with folia not registering recipes correctly

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoitems/items/EcoItemsRecipes.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoitems/items/EcoItemsRecipes.kt
@@ -1,11 +1,14 @@
 package com.willfp.ecoitems.items
 
+import com.willfp.eco.core.Prerequisite
 import com.willfp.eco.core.config.interfaces.Config
 import com.willfp.eco.core.items.Items
 import com.willfp.eco.core.recipe.Recipes
 import com.willfp.libreforge.loader.LibreforgePlugin
 import com.willfp.libreforge.loader.configs.ConfigCategory
 import com.willfp.libreforge.loader.configs.LegacyLocation
+import org.bukkit.Bukkit
+import org.bukkit.entity.Entity
 
 object EcoItemsRecipes : ConfigCategory("recipe", "recipes") {
     override val legacyLocation = LegacyLocation(
@@ -25,10 +28,17 @@ object EcoItemsRecipes : ConfigCategory("recipe", "recipes") {
             item.amount = config.getInt("recipe-give-amount") // Legacy
         }
 
-        plugin.scheduler.runTask {
+        if (Prerequisite.HAS_FOLIA.isMet) {
+            plugin.scheduler.runTask(Bukkit.getOnlinePlayers().map { it as Entity }) {
+                Recipes.createAndRegisterRecipe(
+                    plugin, id, item, config.getStrings("recipe"), config.getStringOrNull("permission")
+                )
+            }
+        } else {
             Recipes.createAndRegisterRecipe(
                 plugin, id, item, config.getStrings("recipe"), config.getStringOrNull("permission")
             )
         }
+
     }
 }
